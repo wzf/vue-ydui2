911c624c663dfc35b5ff2ad0f5f50af454adf023
wzf <feng4job@gmail.com>
2019-06-16 18:05:49 +0800

    toast.error点击可以消失

--- src/components/dialog/src/toast/toast.vue
+++ src/components/dialog/src/toast/toast.vue
@@ -1,28 +1,32 @@
 <template>
-    <div class="yd-dialog-white-mask">
-        <div class="yd-toast" :class="iconsClass == '' ? 'yd-toast-none-icon' : ''">
-            <div v-if="iconsClass" :class="iconsClass"></div>
-            <p class="yd-toast-content" v-html="mes"></p>
-        </div>
+  <div class="yd-dialog-white-mask">
+    <div class="yd-toast"
+         :class="iconsClass == '' ? 'yd-toast-none-icon' : ''">
+      <div v-if="iconsClass"
+           :class="iconsClass"
+           @click="clickIconClose"></div>
+      <p class="yd-toast-content"
+         v-html="mes"></p>
     </div>
+  </div>
 </template>
 
 <script type="text/babel">
-    export default {
-        props: {
-            mes: String,
-            icon: String,
-            timeout: Number,
-            callback: Function
-        },
-        computed: {
-            iconsClass() {
-                let _icon = '';
-                if (this.icon === 'success' || this.icon === 'error') {
-                    _icon = 'yd-toast-' + this.icon + '-icon';
-                }
-                return _icon;
-            }
-        }
+export default {
+  props: {
+    mes: String,
+    icon: String,
+    timeout: Number,
+    callback: Function
+  },
+  computed: {
+    iconsClass() {
+      let _icon = '';
+      if (this.icon === 'success' || this.icon === 'error') {
+        _icon = 'yd-toast-' + this.icon + '-icon';
+      }
+      return _icon;
     }
+  }
+};
 </script>
