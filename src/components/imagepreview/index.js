a0eeeff6c573464af8240d43fb36891bcbf2f157
wzf <feng4job@gmail.com>
2019-12-28 11:59:33 +0800

    通过`this.$closeImagePreview()`退出大图预览

--- src/components/imagepreview/index.js
+++ src/components/imagepreview/index.js
@@ -2,32 +2,39 @@ import ImagePreviewBox from './src/box.vue';
 import ImagePreviewImg from './src/preview-img.vue';
 
 const install = function(Vue) {
-    const boxComponent = Vue.extend(ImagePreviewBox);
-    const imgComponent = Vue.extend(ImagePreviewImg);
+  const boxComponent = Vue.extend(ImagePreviewBox);
+  const imgComponent = Vue.extend(ImagePreviewImg);
 
-    // 添加vue扩展，方法名imagePreview
-    Vue.prototype.$imagePreview = ({index = 0, images}) => {
-        let imageItems = images.map(img => {
-            return new imgComponent({
-                el: document.createElement('div'),
-                data: {
-                    src: img
-                }
-            });
-        });
-        const instance = new boxComponent({
-            el: document.createElement('div'),
-            data: {
-                index: index,
-                currentIndex: index,
-                imgItems:imageItems,
-                txtHTML: /*mes[0] && mes[0].$el ? mes[0].$el.innerHTML :*/ '',
-                closeText: '关闭'
-            }
-        });
-
-        document.body.appendChild(instance.$el);
+  // 添加vue扩展，方法名imagePreview
+  Vue.prototype.$imagePreview = ({ index = 0, images }) => {
+    let imageItems = images.map(img => {
+      return new imgComponent({
+        el: document.createElement('div'),
+        data: {
+          src: img
+        }
+      });
+    });
+    const instance = new boxComponent({
+      el: document.createElement('div'),
+      data: {
+        index: index,
+        currentIndex: index,
+        imgItems: imageItems,
+        txtHTML: /*mes[0] && mes[0].$el ? mes[0].$el.innerHTML :*/ '',
+        closeText: '关闭'
       }
-}
+    });
 
-export default {install};
+    document.body.appendChild(instance.$el);
+  };
+
+  Vue.prototype.$closeImagePreview = () => {
+    const instance = document.querySelector('.yd-lightbox-view');
+    if (instance) {
+      document.body.removeChild(instance);
+    }
+  };
+};
+
+export default { install };
